12ddb53a55abb44bd56b6e3b75d3763084299375
Jose <josedmym@gmail.com>
2022-10-15 10:33:55 -0600

    Prueba1

--- Lab5_Jose_Munoz_1209121/MyForm.h
+++ Lab5_Jose_Munoz_1209121/MyForm.h
@@ -8,7 +8,7 @@ namespace Lab5JoseMunoz1209121 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
-
+	using namespace System::IO;
 	/// <summary>
 	/// Resumen de MyForm
 	/// </summary>
@@ -34,6 +34,16 @@ namespace Lab5JoseMunoz1209121 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	protected:
+	private: System::Windows::Forms::ListBox^ listBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +58,148 @@ namespace Lab5JoseMunoz1209121 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(0, -1);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(503, 351);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->Location = System::Drawing::Point(504, 101);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(146, 251);
+			this->listBox1->TabIndex = 1;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(512, 12);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Ordenar";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(512, 59);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 3;
+			this->button2->Text = L"ordenar";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(509, 38);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(85, 13);
+			this->label1->TabIndex = 4;
+			this->label1->Text = L"(Por generacion)";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(509, 85);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(137, 13);
+			this->label2->TabIndex = 5;
+			this->label2->Text = L"(Por No. Pokedex nacional)";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(83, 18);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(345, 33);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"Ordenador de Pokemon";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(651, 348);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->pictureBox1);
+			this->Name = L"MyForm";
+			this->Text = L"POKEMON!";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e)
+	{
+		//Ordenar por
+		String^ fileName = "Prueba.text";
+		try
+		{
+			FileStream^ fs = gcnew FileStream(fileName, FileMode::Open);
+			BinaryReader^ br = gcnew BinaryReader(fs);
+
+			Console::WriteLine("contents of {0}:", fileName);
+			while (br->BaseStream->Position < br->BaseStream->Length)
+				Console::WriteLine(br->ReadInt32().ToString());
+
+			fs->Close();
+		}
+		catch (Exception^ e)
+		{
+			if (dynamic_cast<FileNotFoundException^>(e))
+				Console::WriteLine("File '{0}' not found", fileName);
+			else
+				Console::WriteLine("Exception: ({0})", e);
+		}
+
+	}
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+		String^ fileName = "Prueba.text";
+		try
+		{
+			FileStream^ fs = gcnew FileStream(fileName, FileMode::Open);
+			BinaryReader^ br = gcnew BinaryReader(fs);
+
+			Console::WriteLine("contents of {0}:", fileName);
+			while (br->BaseStream->Position < br->BaseStream->Length)
+				Console::WriteLine(br->ReadInt32().ToString());
+			label1->Text = (br->ReadInt32().ToString());
+			fs->Close();
+		}
+		catch (Exception^ e)
+		{
+			if (dynamic_cast<FileNotFoundException^>(e))
+				Console::WriteLine("File '{0}' not found", fileName);
+			else
+				Console::WriteLine("Exception: ({0})", e);
+		}
+	}
+};
 }
